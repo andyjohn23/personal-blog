d44c2416bd21c38c9c8150e33602219cda437156
andyjohn23 <andrewnick906@gmail.com>
2020-11-02 18:16:00 +0300

    updated files

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,7 +3,7 @@ from . import auth
 from ..models import User
 from .forms import RegistrationForm,LoginForm,adminForm
 from .. import db
-from flask_login import login_user,logout_user,login_required
+from flask_login import login_user,logout_user,login_required,role_required
 
 
 @auth.route('/register',methods = ["GET","POST"])
